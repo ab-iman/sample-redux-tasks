64e903ebcf744f49def65363be6c2084f8ebebdf
Adarsh Bhat <adarsh.bhat@imanage.com>
2019-02-15 15:26:38 -0600

    Revert "Missed one file"
    This reverts commit f3169506d30296f5ce2842fa0d6ef8feb4599a28.

--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { List, List } from 'immutable';
-import { Task } from '../models';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { List } from "immutable";
+import { Task } from "../models";
 
 @Component({
   selector: 'app-task-list',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
 
+  @Input() tasks: List<Task>;
+  @Output() toggle = new EventEmitter<Task>();
+  @Output() remove = new EventEmitter<Task>();
+
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  toggleTask(task: Task) {
+    this.toggle.emit(task);
+  }
+
+  removeTask(task: Task) {
+    this.remove.emit(task);
+    return false;
+  }
+
 
 }
